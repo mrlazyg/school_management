ddddafa58a43ef24f996572c4246e5bc66573792
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2023-09-16 15:30:39 +0530

    Resolver and Repository

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,18 +1,22 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(() => LessonType)
-  lesson() {
-    return {
-      id: 'putin',
-      name: 'Chemistry Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(() => LessonType)
-  createLesson() {}
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
 }
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Lesson } from './lesson.entity';
+
+const logger = new Logger('LessonService');
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepo: Repository<Lesson>,
+  ) {}
+
+  async getLesson(id): Promise<Lesson> {
+    logger.log(`Get a lesson. Query: ${JSON.stringify({ id })}`);
+    return this.lessonRepo.findOne({
+      where: { id },
+    });
+  }
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonRepo.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    logger.log(`Create a lesson. Payload: ${JSON.stringify(lesson)}`);
+    return this.lessonRepo.save(lesson);
+  }
+}
